9f67d0a1c54f168d9a84469364b7d8dc47807476
Ignacy Sokołowski <ignacy.sokolowski@gmail.com>
2012-06-30 23:16:29 +0200

    Added jumping to the next number on the line - fixes #1

--- vintage_numbers.py
+++ vintage_numbers.py
@@ -2,6 +2,7 @@
 Vintage Numbers plugin for Sublime Text 2
 by Ignacy Sokolowski
 """
+import re
 import sublime
 import sublime_plugin
 
@@ -24,12 +25,16 @@ class ViNumberMixin(object):
         for s in reversed(self.view.sel()):
             # Getting caret position.
             (row, col) = self.view.rowcol(s.begin())
-            # Getting line and char at the caret.
-            line = self.view.substr(self.view.line(self.view.text_point(row, 0)))
-            char = line[col]
+            # Getting line at the caret.
+            line = self.view.substr(
+                        self.view.line(self.view.text_point(row, 0)))
 
-            if s.empty() and char.isdigit():
-                # Getting begining and end columns of the whole number position.
+            if not s.empty() or len(line) == 0:
+                continue
+
+            if line[col].isdigit():
+                # Getting begining and end columns of
+                # the whole number position.
                 beg, end = (col, col + 1)
                 while beg >= 0 and line[beg:end].isdigit():
                     beg -= 1
@@ -37,23 +42,38 @@ class ViNumberMixin(object):
                 while end <= len(line) and line[beg:end].isdigit():
                     end += 1
                 end -= 1
+            else:
+                # Finding the next number in the current line.
+                match = re.search(r'\d+', line[col:])
+                if not match:
+                    continue
+                # Getting begining and end columns of number position.
+                beg, end = map(lambda x: x + col, match.span())
 
-                # Incrementing/decrementing the number and converting to string.
-                number = str(int_method(int(line[beg:end]), 1))
+            # Incrementing/decrementing the number and
+            # converting to string.
+            number = line[beg:end]
+            new_number = str(int_method(int(number), 1))
 
-                # Counting region that will be replaced.
-                start = s.b - (col - beg)
-                stop = s.b - (col - end)
+            # Counting region that will be replaced.
+            start = s.b - (col - beg)
+            stop = s.b - (col - end)
 
-                # Replacing number at the caret
-                # with the incremented/decremented one.
-                self.view.replace(edit, sublime.Region(start, stop), number)
+            # Replacing number at the caret
+            # with the incremented/decremented one.
+            self.view.replace(edit, sublime.Region(start, stop), new_number)
 
-            new_sel.append(s)
+            # Moving the caret to the end of the number.
+            if len(number) == len(new_number):
+                stop -= 1
+            elif len(number) > len(new_number):
+                stop -= 2
+            new_sel.append(sublime.Region(stop, stop))
 
-        self.view.sel().clear()
-        for s in new_sel:
-            self.view.sel().add(s)
+        if new_sel:
+            self.view.sel().clear()
+            for s in new_sel:
+                self.view.sel().add(s)
 
 
 class ViIncrementNumberCommand(sublime_plugin.TextCommand, ViNumberMixin):
